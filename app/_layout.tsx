71967bc5f75d83726f2252a795680b2426f216d2
tricia <triciachew18@gmail.com>
2025-05-17 13:26:35 +0800

    set up new expo app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,10 +20,12 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(main)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+
+
