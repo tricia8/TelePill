6313e38b633585e970d6814f006a7c53a3d8823a
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-05-18 01:35:11 +0800

    fix: removed Card component, updated article display, and added error logic for rendering news feed

--- app/(main)/(tabs)/index.tsx
+++ app/(main)/(tabs)/index.tsx
@@ -9,6 +9,8 @@ import {
   ActivityIndicator,
   Modal,
   Pressable,
+  Image,
+  Linking,
 } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { Ionicons } from "@expo/vector-icons";
@@ -19,13 +21,22 @@ import { SearchBar } from "react-native-screens";
 import { fetchHealthNewsByCountry } from "@/lib/api/api";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { Card, Icon, Overlay } from "react-native-elements";
+import { QueryClientProvider, useQuery } from "@tanstack/react-query";
 
 export default function HomeScreen() {
-  const [news, setNews] = useState([]);
-  const [selectedNews, setSelectedNews] = useState(null);
-  const { country } = useLocalSearchParams(); // extracts country parameter from the URL parameters
+  // const [news, setNews] = useState([]);
+  interface NewsArticle {
+    author: string;
+    title: string;
+    url: string;
+    urlToImage?: string;
+    description?: string;
+    content?: string;
+  }
+
+  const [selectedNews, setSelectedNews] = useState<NewsArticle | null>(null);
+  // const { country } = useLocalSearchParams(); // extracts country parameter from the URL parameters (or: retrieve from firebase)
+  const country = "us"; //dummy country
   const [modalVisible, setModalVisible] = useState(false);
   const { data, error, isError, isSuccess, isLoading } = useQuery({
     queryKey: ["newsByCategory"],
@@ -38,7 +49,14 @@ export default function HomeScreen() {
     fetchNews();
   }, [selectedCountry]); */
 
-  const renderItem = () => {};
+  /* if (isError) {
+    return (
+      <View style={styles.error}>
+        <Text>Error: {error?.message}</Text>
+      </View>
+    );
+  } */
+
   return (
     <SafeAreaProvider style={styles.container}>
       <ScrollView>
@@ -64,6 +82,10 @@ export default function HomeScreen() {
 
               {isLoading ? (
                 <ActivityIndicator size="large" />
+              ) : isError ? (
+                <View style={styles.error}>
+                  <Text>Error: {error.message}</Text>
+                </View>
               ) : (
                 <FlatList
                   data={data.articles}
@@ -76,30 +98,19 @@ export default function HomeScreen() {
                           setSelectedNews(item);
                           setModalVisible(true);
                         }}
-
-                        /* router.push({
-                            pathname: `/article`,
-                            params: {
-                              title: item.title,
-                              desc: item.description,
-                              content: item.content,
-                              urlToImage: item.urlToImage,
-                            },
-                          }) */
                       >
-                        <Card containerStyle={styles.cardContainer}>
-                          <Card.Title>{item.title}</Card.Title>
-                          <Card.Divider />
-                          {item.urlToImage && (
-                            <Card.Image
-                              source={{ uri: item.urlToImage }}
-                              style={styles.cardImage}
-                            />
-                          )}
-                          <Text style={styles.articleDescription}>
-                            {item.description}
-                          </Text>
-                        </Card>
+                        <ThemedText type="defaultSemiBold">
+                          {item.title}
+                        </ThemedText>
+                        {item.urlToImage && (
+                          <Image
+                            source={{ uri: item.urlToImage }}
+                            style={styles.cardImage}
+                          />
+                        )}
+                        <Text style={styles.articleDescription}>
+                          {item.description}
+                        </Text>
                       </TouchableOpacity>
                     );
                   }}
@@ -115,7 +126,10 @@ export default function HomeScreen() {
               }}
             >
               <View>
-                <View>
+                <View style={{ flexDirection: "row" }}>
+                  <ThemedText type="defaultSemiBold">
+                    {selectedNews?.title}
+                  </ThemedText>
                   <Pressable>
                     <Ionicons
                       name="checkmark-circle"
@@ -125,6 +139,23 @@ export default function HomeScreen() {
                     />
                   </Pressable>
                 </View>
+                <Image
+                  source={{ uri: selectedNews?.urlToImage }}
+                  style={styles.cardImage}
+                />
+                <Text style={styles.articleDescription}>
+                  {selectedNews?.content}
+                </Text>
+                <TouchableOpacity
+                  style={styles.readMoreButton}
+                  onPress={() => {
+                    if (selectedNews?.url) {
+                      Linking.openURL(selectedNews.url);
+                    }
+                  }}
+                >
+                  <Text>Read Full Article</Text>
+                </TouchableOpacity>
               </View>
             </Modal>
           </ThemedView>
@@ -179,6 +210,7 @@ const styles = StyleSheet.create({
   articleContainer: {
     borderRadius: 10,
     padding: 15,
+    backgroundColor: "#5faaff",
   },
   cardContainer: {
     borderRadius: 8,
@@ -205,5 +237,12 @@ const styles = StyleSheet.create({
   smallText: {
     fontSize: RFValue(15),
     fontStyle: "italic",
+  },
+  error: {},
+  readMoreButton: {
+    padding: 8,
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: "center",
   },
 });
