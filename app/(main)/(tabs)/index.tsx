5e352d790bb2ee3cebc2c3f1be04cbd370e068f9
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-05-17 18:44:45 +0800

    feat: updated homepage with API integration for health newsfeed

--- app/(main)/(tabs)/index.tsx
+++ app/(main)/(tabs)/index.tsx
@@ -5,14 +5,40 @@ import {
   View,
   Text,
   TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+  Modal,
+  Pressable,
 } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { Ionicons } from "@expo/vector-icons";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SearchBar } from "react-native-screens";
+import { fetchHealthNewsByCountry } from "@/lib/api/api";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Card, Icon, Overlay } from "react-native-elements";
 
 export default function HomeScreen() {
+  const [news, setNews] = useState([]);
+  const [selectedNews, setSelectedNews] = useState(null);
+  const { country } = useLocalSearchParams(); // extracts country parameter from the URL parameters
+  const [modalVisible, setModalVisible] = useState(false);
+  const { data, error, isError, isSuccess, isLoading } = useQuery({
+    queryKey: ["newsByCategory"],
+    queryFn: () => fetchHealthNewsByCountry(country),
+  });
+
+  const router = useRouter();
+
+  /* useEffect(() => {
+    fetchNews();
+  }, [selectedCountry]); */
+
+  const renderItem = () => {};
   return (
     <SafeAreaProvider style={styles.container}>
       <ScrollView>
@@ -29,11 +55,78 @@ export default function HomeScreen() {
               Latest News
             </ThemedText>
             <ThemedText style={[styles.smallText]}>
-              Your daily dose of health news in your area
+              Your daily dose of health news in your area, or anywhere of
+              interest
             </ThemedText>
             <View>
-              <Text>News Feed Placeholder</Text>
+              <SearchBar></SearchBar>
+              {/* Drop down picker for country default curr location*/}
+
+              {isLoading ? (
+                <ActivityIndicator size="large" />
+              ) : (
+                <FlatList
+                  data={data.articles}
+                  keyExtractor={(item) => item.url}
+                  renderItem={({ item }) => {
+                    return (
+                      <TouchableOpacity
+                        style={styles.articleContainer}
+                        onPress={() => {
+                          setSelectedNews(item);
+                          setModalVisible(true);
+                        }}
+
+                        /* router.push({
+                            pathname: `/article`,
+                            params: {
+                              title: item.title,
+                              desc: item.description,
+                              content: item.content,
+                              urlToImage: item.urlToImage,
+                            },
+                          }) */
+                      >
+                        <Card containerStyle={styles.cardContainer}>
+                          <Card.Title>{item.title}</Card.Title>
+                          <Card.Divider />
+                          {item.urlToImage && (
+                            <Card.Image
+                              source={{ uri: item.urlToImage }}
+                              style={styles.cardImage}
+                            />
+                          )}
+                          <Text style={styles.articleDescription}>
+                            {item.description}
+                          </Text>
+                        </Card>
+                      </TouchableOpacity>
+                    );
+                  }}
+                ></FlatList>
+              )}
             </View>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <View>
+                <View>
+                  <Pressable>
+                    <Ionicons
+                      name="checkmark-circle"
+                      size={32}
+                      color="green"
+                      onPress={() => setModalVisible(!modalVisible)}
+                    />
+                  </Pressable>
+                </View>
+              </View>
+            </Modal>
           </ThemedView>
 
           <ThemedView style={styles.newsContainer}>
@@ -83,6 +176,21 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
   },
+  articleContainer: {
+    borderRadius: 10,
+    padding: 15,
+  },
+  cardContainer: {
+    borderRadius: 8,
+    elevation: 3,
+    marginBottom: 15,
+  },
+  cardImage: {
+    height: 200,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  articleDescription: {},
   listContainer: {
     gap: 8,
     padding: 10,
